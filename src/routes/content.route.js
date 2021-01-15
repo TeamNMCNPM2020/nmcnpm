03cd29fbd20de52311620b26de80ad95893caf59
Lê Nguyên Hào <18120355@student.hcmus.edu.vn>
2021-01-15 18:55:07 +0700

    Finish content view + fix minor UI errors

--- src/routes/content.route.js
+++ src/routes/content.route.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
-router.get('/edit', function(req, res) {
+router.get('/edit', function (req, res) {
   res.render('content_edit');
+});
+
+router.get('/:id', function (req,res) {
+  res.render('content');
 });
 
 module.exports = router;
